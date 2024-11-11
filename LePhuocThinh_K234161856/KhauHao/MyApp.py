c7e7d7d877aa83811ac1938a62ff7b20d391195f
Anna-PhuocThinh <thinhlp23416@st.uel.edu.vn>
2024-11-11 19:13:47 +0700

    Delete LePhuocThinh_K234161856 directory

--- LePhuocThinh_K234161856/KhauHao/MyApp.py
+++
@@ -1,9 +0,0 @@
-from PyQt6.QtWidgets import QApplication, QMainWindow, QHBoxLayout
-
-from LePhuocThinh_K234161856.KhauHao.MainWindowKhauHaoExt import MainWindowKhauHaoExt
-
-app=QApplication([])
-myWindow= MainWindowKhauHaoExt()
-myWindow.setupUi(QMainWindow())
-myWindow.show()
-app.exec()
